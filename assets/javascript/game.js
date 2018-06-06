4f88272f2412c15618d2b23824853553bf058fba
chrissydc <bychriscelle@gmail.com>
2018-06-05 21:48:07 -0400

    html updated, layout built, js var stated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-    
-})
+var myList = ["Japan", "Germany", "Korea", "China", "France", "Hawaii", "Russia", "United States", "Philippines", "India", "Spain", "Brazil"];
+console.log(myList);
+
+var myGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log(myGuesses);
+
+var blanks = "_";
+console.log(blanks);
+
